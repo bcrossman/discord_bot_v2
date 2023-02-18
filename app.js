d57e73b2a970497a347d5e858339f3e7a356282f
Brent Crossman <brent.crossman@gmail.com>
2023-02-18 15:10:37 -0500

    working slash

--- app.js
+++ app.js
@@ -1,24 +1,47 @@
-const Discord = require('discord.js');
-require('dotenv').config();
+const fs = require('node:fs');
+const path = require('node:path');
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js');
+const { token } = require('./config.json');
 
-const { Client, GatewayIntentBits } = require('discord.js');
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] });
 
-const client = new Client({
-	intents: [
-		GatewayIntentBits.Guilds,
-		GatewayIntentBits.GuildMessages,
-		GatewayIntentBits.MessageContent,
-		GatewayIntentBits.GuildMembers,
-	],
+client.commands = new Collection();
+const commandsPath = path.join(__dirname, 'commands');
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const filePath = path.join(commandsPath, file);
+	const command = require(filePath);
+	client.commands.set(command.data.name, command);
+}
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+	console.log(`Ready! Logged in as ${c.user.tag}`);
 });
 
-client.on('ready', () => {
+client.on(Events.InteractionCreate, async interaction => {
+	if (!interaction.isChatInputCommand()) return;
 
-	client.on('message', (msg) => {
-		if (msg.content === 'Hello') msg.reply('Hi');
-	});
-	console.log('The Bot is ready!');
+	const command = client.commands.get(interaction.commandName);
+
+	if (!command) return;
+
+	try {
+		await command.execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true });
+	}
 });
 
-client.login(process.env.BOT_TOKEN);
+// Log in to Discord with your client's token
+client.login(token);
 
+client.on('ready', () => console.log('The Bot is ready!'));
+
+client.on('message', (msg) => {
+  if (msg.content === 'Hello') msg.reply('Hi');
+});

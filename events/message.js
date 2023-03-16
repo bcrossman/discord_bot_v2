787a12b1bf808d745e627f338411c8fee6d9f17f
Brent Crossman <brent.crossman@gmail.com>
2023-03-15 21:18:23 -0400

    big update for charts on play times

--- events/message.js
+++ events/message.js
@@ -1,4 +1,5 @@
-const { Events } = require('discord.js');
+const { Discord } = require('discord.js');
+const { ChartJSNodeCanvas } = require('chartjs-node-canvas');
 const fs = require('fs');
 const path = require('path');
 
@@ -16,6 +17,65 @@ const urls = [
 
 const filePath_bug = path.join(__dirname, '..', 'data', 'bug-states.json');
 const filePath_advance = path.join(__dirname, '..', 'data', 'advance-states.json');
+const filePath_played = path.join(__dirname, '..', 'data', 'played-states.json');
+
+function getHoursCount(times) {
+  const hoursCount = new Array(24).fill(0);
+  times.forEach((time) => {
+    const date = new Date(time);
+    const hour = date.getHours();
+    hoursCount[hour]++;
+  });
+  return hoursCount;
+}
+
+function createChartConfiguration(labels, data) {
+  return {
+    type: 'bar',
+    data: {
+      labels: labels,
+      datasets: [
+        {
+          label: 'Times Played',
+          data: data,
+          backgroundColor: 'rgba(75, 192, 192, 0.2)',
+          borderColor: 'rgba(75, 192, 192, 1)',
+          borderWidth: 1,
+        },
+      ],
+    },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true,
+        },
+      },
+    },
+  };
+}
+
+async function sendBarChart(message, authorKey) {
+  const userId = authorKey
+  const filePath_played = path.join(__dirname, '..', 'data', 'played-states.json');
+  const jsonData = readStates(filePath_played);
+
+  if (!jsonData[userId]) {
+    return message.channel.send("No data found for this user.");
+  }
+
+  const timestamps = jsonData[userId];
+  const hours = getHoursCount(timestamps);
+
+  // Create an array of hour labels
+  const hourLabels = Array.from({length: 24}, (_, i) => i.toString());
+
+  const chartJSNodeCanvas = new ChartJSNodeCanvas({ width: 600, height: 400 });
+  const configuration = createChartConfiguration(hourLabels, hours);
+  const image = await chartJSNodeCanvas.renderToBuffer(configuration);
+    // Save the image to the data folder
+  const imagePath = path.join(__dirname, '..', 'data', `chart.png`);
+  await fs.promises.writeFile(imagePath, image);
+}
 
 function writeStates(states, filePath) {
     fs.writeFileSync(filePath, JSON.stringify(states, null, 2));
@@ -31,7 +91,7 @@ function readStates(filePath) {
 }
 
 module.exports = {
-    name: Events.MessageCreate,
+    name: 'messageCreate',
     execute: async function (message) {
 		        if (message.content.toLowerCase() === 'gg'&message.author.id==='705579756380356629') {
             try {
@@ -143,6 +203,33 @@ module.exports = {
                 });
             }
         }
+		if (message.content.toLowerCase().includes('is live over at')) {
+			  try {
+				const playedStates = readStates(filePath_played);
+				const words = message.content.split(' ');
+				const phraseIndex = words.findIndex(word => word.toLowerCase() === 'is') - 1;
+				const authorKey = phraseIndex >= 0 ? words[phraseIndex] : '';
+				const times = playedStates[authorKey] ?? [];
+				const easternTime = message.createdAt.toLocaleString('en-US', { timeZone: 'America/New_York' });
+				times.push(easternTime);
+				playedStates[authorKey] = times;
+				writeStates(playedStates, filePath_played);
+				await sendBarChart(message, authorKey);
+				const imagePath = path.join(__dirname, '..', 'data', `chart.png`);
+				await message.channel.send({
+								files: [{
+									attachment: imagePath,
+									name: 'chart.png'
+								  }]
+								});
+				} catch (error) {
+				console.error(error);
+				await message.reply({
+				  content: "Didn't work!",
+				  ephemeral: true,
+				});
+			  }
+			}
 		if (message.content.toLowerCase() === "Levi's sucks") {
             try {
                 await message.reply('https://giphy.com/gifs/Friends-season-6-episode-624-the-one-with-proposal-part-1-Js1ur5v1vmaj4mRxyq');
